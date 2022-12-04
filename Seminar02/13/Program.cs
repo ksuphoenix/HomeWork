be602991f373e0aaafeb01c775547e8bf3b522ec
ksuphoenix <ksuphoenix@gmail.com>
2022-12-04 15:01:54 +0300

    13

--- Seminar02/13/Program.cs
+++ Seminar02/13/Program.cs
@@ -7,7 +7,7 @@ int num = int.Parse(Console.ReadLine());
 
 int i = 100;
 int k = 1;
- 
+int result = 0; 
 
 if (num<i)
 {
@@ -15,16 +15,12 @@ if (num<i)
 }
 else 
 {
-    i = i*10;
-    k=k*10;
-    while (num<i) 
+    while (i<num) 
     {
-    if (((i*10)>num) && (num>(i-1)))
-    {
-        Console.WriteLine($"{(num/k)%10}");
-    }
-    
-    i = i*10;
-    k=k*10;
+        result = (num/k)%10;
+        i = i*10; 
+        k=k*10;
     }
 }
+
+Console.WriteLine(result);
