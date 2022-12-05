8ee4c5831efc2d04fc8123d8900165b2c152e059
ksuphoenix <ksuphoenix@gmail.com>
2022-12-05 20:28:39 +0300

    17

---
+++ Seminar03/17/Program.cs
@@ -0,0 +1,26 @@
+﻿// 
+Console.Write($"x=");
+int x = int.Parse(Console.ReadLine());
+Console.Write($"y=");
+int y = int.Parse(Console.ReadLine());
+
+if (x>0 && y>0) 
+{
+    Console.WriteLine($"1");
+}
+if (x<0 && y>0) 
+{
+    Console.WriteLine($"2");
+}
+if (x<0 && y<0) 
+{
+    Console.WriteLine($"3");
+}
+if (x>0 && y<0) 
+{
+    Console.WriteLine($"4");
+}
+if (x==0 || y==0) 
+{
+    Console.WriteLine($"На оси");
+}
