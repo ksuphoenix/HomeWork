6175f4cdf43d3b905848b989eeb00a73d31cfaae
ksuphoenix <ksuphoenix@gmail.com>
2022-12-07 11:41:41 +0300

    27

---
+++ Seminar04/27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.Write ("Введите число: ");
+int N = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"Сумма цифр в числе {N} равна {GetNumberSum (N)}");
+
+int GetNumberSum (int N) 
+{
+    int sum = 0;
+    for (int i = 1; i <= N; i = i*10)
+        sum += (N/i)%10;
+    return sum;
+}
