60846cdb0b6257a41e8322aa793b82a941aa546e
ksuphoenix <ksuphoenix@gmail.com>
2022-12-07 11:26:17 +0300

    25

---
+++ Seminar04/25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.Write ("Введите число A: ");
+int A = int.Parse (Console.ReadLine ());
+Console.Write ("Введите число B: ");
+int B = int.Parse (Console.ReadLine ());
+Console.WriteLine ($"Число {A} в степени {B} равно {GetStepen (A,B)}");
+
+int GetStepen (int A, int B) 
+{
+    if (B == 0)
+    {
+        int stp = 1;
+        return stp;
+    }
+    else 
+    {
+        int st = 1;
+        for (int i = 1; i <= B; i++)
+        st = st*A;
+        return st;
+    }
+}
