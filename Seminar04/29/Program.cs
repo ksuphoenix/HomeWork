e9945a70970e6801d2922df1487bfa0c12e62945
ksuphoenix <ksuphoenix@gmail.com>
2022-12-07 11:45:16 +0300

    29

---
+++ Seminar04/29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+Console.WriteLine($"[{String.Join(",", NewArray(8))}]");
+
+int[] NewArray(int index)
+{
+    int[] arr = new int [index];
+    for  (int i =0; i<index; i++)
+    {
+        arr[i] = new Random().Next(0,100);
+    }
+    return arr;
+}
