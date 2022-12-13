b620cd11cbc9b4bc4dc1e0036a7ee6fa15c5a1b2
ksuphoenix <ksuphoenix@gmail.com>
2022-12-13 19:38:08 +0300

    double array

---
+++ Seminar07/47/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * 5 - 2;;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]:f2}   ");
+        }
+        WriteLine();
+    }
+}
